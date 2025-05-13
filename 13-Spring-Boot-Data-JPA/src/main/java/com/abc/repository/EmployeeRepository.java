548c3cf63d467eb08c5611335f68d7191b5fc2a0
hitu-1995 <hiteshwankhede11@gmail.com>
2025-05-13 08:52:38 +0530

    13-05-2025

--- 13-Spring-Boot-Data-JPA/src/main/java/com/abc/repository/EmployeeRepository.java
+++ 13-Spring-Boot-Data-JPA/src/main/java/com/abc/repository/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.abc.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.abc.model.Employee;
@@ -11,4 +14,22 @@ import com.abc.model.Employee;
 public interface EmployeeRepository  extends JpaRepository<Employee, Integer>{
 
 	   List<Employee> findByMyName(String name);
+	   
+	   List<Employee> findDistinctByAge(int age);
+	   
+	   List<Employee> findByAgeAndSalary(int age, double salary);
+	   
+	   List<Employee> findBySalaryBetween(double salary1, double salary2);
+	   
+	   List<Employee> findBySalaryGreaterThan(double salary);
+	   
+	   List<Employee> findBySalaryGreaterThanEqual(double salary);
+
+	   List<Employee> findByMyNameLike(String pattern);
+
+	   @Query(value = "select e from Employee e where e.age = :age")
+	  Employee findByAge(@Param("age") int age);
+	   
+	   // mobile, email, id, username
+	   
 }
--- 13-Spring-Boot-Data-JPA/src/main/java/com/abc/service/EmployeeServiceIMPL.java
+++ 13-Spring-Boot-Data-JPA/src/main/java/com/abc/service/EmployeeServiceIMPL.java
@@ -18,8 +18,44 @@ public class EmployeeServiceIMPL {
 	private EmployeeRepository repository;
 
 	public void check() {
- 
-          List<Employee> list = repository.findByMyName("ABC");
+
+		System.out.println(repository.findByAge(31));
+		
+
+	}
+
+	private void getNameLike() {
+		repository.findByMyNameLike("A%")
+		.forEach(e -> System.out.println(e));
+	}
+
+	private void getSalaryGreaterThanEqual() {
+		repository.findBySalaryGreaterThanEqual(40000)
+			.forEach(e -> System.out.println(e));
+	}
+
+	private void getSalaryGreaterThan() {
+		repository.findBySalaryGreaterThan(40000)
+			.forEach(e -> System.out.println(e));
+	}
+
+	private void getSalaryBetween() {
+		repository.findBySalaryBetween(30000, 85000)
+		  			.forEach(e -> System.out.println(e));
+	}
+
+	private void getByAge() {
+		List<Employee> list = repository.findByAgeAndSalary(18, 40000);
+		  list.forEach(e -> System.out.println(e));
+	}
+
+	private void getDistinctByAge() {
+		List<Employee> list = repository.findDistinctByAge(31);
+		  list.forEach(e -> System.out.println(e));
+	}
+
+	private void getByName() {
+		List<Employee> list = repository.findByMyName("ABC");
           for(Employee emp :list)
           { 
         	 System.out.println(emp);  
@@ -31,11 +67,13 @@ public class EmployeeServiceIMPL {
 
 		List<Employee> list = new ArrayList<Employee>();
 		list.add(new Employee(101, "ABC", 90000, 34));
-		list.add(new Employee(102, "XYZ", 80000, 31));
+		list.add(new Employee(102, "XYZ", 20000, 31));
+		list.add(new Employee(102, "JKL", 50000, 31));
+		list.add(new Employee(102, "AEF", 80000, 31));
 		list.add(new Employee(103, "PQR", 20000, 23));
-		list.add(new Employee(104, "TUV", 40000, 18));
+		list.add(new Employee(104, "ADC", 40000, 18));
 		list.add(new Employee(105, "LMN", 70000, 45));
-		list.add(new Employee(106, "ABC", 70000, 35));
+		list.add(new Employee(106, "ABC", 40000, 18));
 
 		repository.saveAll(list);
 
